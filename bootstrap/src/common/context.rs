38e5843fef104f7445169f7bbcdf792f924e2257
Dao Ming Chuo <dominique@elodie.dev>
2024-12-18 23:21:47 +0800

    Removes duplicated Context

--- bootstrap/src/common/context.rs
+++ bootstrap/src/common/context.rs
@@ -1,4 +1,4 @@
-use crate::common::{StringTable, TypeTable};
+use crate::common::{StringTable, StringTableId, TypeTable};
 use crate::common::symbol::SymbolTable;
 
 #[derive(Debug)]
@@ -6,4 +6,17 @@ pub struct Context {
     pub string_table: StringTable,
     pub symbol_table: SymbolTable,
     pub type_table: TypeTable,
+}
+
+impl Context {
+
+    pub fn new() -> Self {
+        Self {
+            string_table: StringTable::new(),
+            symbol_table: SymbolTable::new(),
+            type_table: TypeTable::new(),
+        }
+    }
+
+    pub fn get_str(&self, idx: StringTableId) -> &str { self.string_table.get(idx) }
 }
--- bootstrap/src/common/mod.rs
+++ bootstrap/src/common/mod.rs
@@ -1,7 +1,8 @@
+pub use context::Context;
 pub use package::PackagePath;
 pub use r#type::{BaseType, DefaultTypeIds, Property, Type, TypeId, TypeName, TypeTable};
 pub use string::{StringTable, StringTableId};
-pub use symbol::{Symbol, SymbolId, SymbolName};
+pub use symbol::SymbolId;
 pub use util::*;
 
 mod util;
@@ -10,22 +11,3 @@ mod package;
 mod r#type;
 mod symbol;
 mod context;
-
-
-pub struct Context {
-    pub string_table: StringTable,
-    pub type_table: TypeTable,
-}
-
-impl Context {
-    pub fn new() -> Self {
-        Self {
-            string_table: StringTable::new(),
-            type_table: TypeTable::new(),
-        }
-    }
-}
-
-impl Context {
-    pub fn get_str(&self, idx: StringTableId) -> &str { self.string_table.get(idx) }
-}
